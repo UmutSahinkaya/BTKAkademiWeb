a59ad8051c6cf28e23e3c6129513f84fadced731
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-05 09:02:36 +0300

    Add project files.

---
+++ BTKAkademiWeb.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTKAkademiWeb.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
