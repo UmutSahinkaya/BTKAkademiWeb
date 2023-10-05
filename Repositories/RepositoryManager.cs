3c665e24825c9be4b1d1ca6f9306d97842a3300d
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-05 10:12:39 +0300

    Repository Pattern ve UnitOfWork Pattern projede kullanıldı.

---
+++ Repositories/RepositoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class RepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public RepositoryManager(RepositoryContext context, IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _context = context;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IProductRepository Product => _productRepository;
+        public ICategoryRepository Category => _categoryRepository;
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
