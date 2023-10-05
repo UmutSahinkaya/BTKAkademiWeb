7ef71109513a70ea14048188dedc454dac458253
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-05 09:14:09 +0300

    Entityler oluşturuldu.

---
+++ Entities/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public String? ProductName { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
+
