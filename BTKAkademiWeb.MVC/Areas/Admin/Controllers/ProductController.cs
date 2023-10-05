d05cd0688adfbd93eb3e7c1e6bbe6751f37c0bbd
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-05 15:42:08 +0300

    ProductController güncellendi

--- BTKAkademiWeb.MVC/Areas/Admin/Controllers/ProductController.cs
+++ BTKAkademiWeb.MVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 
 namespace BTKAkademiWeb.MVC.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly IServiceManager _manager;
+
+        public ProductController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
         public IActionResult Index()
+        {
+            var model = _manager.ProductService.GetAllProducts(false);
+            return View(model);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
