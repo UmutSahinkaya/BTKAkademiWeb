ebaf4fccc8cbda6f140169d2904403085785e9a5
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-06 12:45:34 +0300

    Session işlemleri

--- BTKAkademiWeb.MVC/Pages/Demo.cshtml.cs
+++ BTKAkademiWeb.MVC/Pages/Demo.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BTKAkademiWeb.MVC.Pages
+namespace   BTKAkademiWeb.MVC.Pages
 {
     public class DemoModel : PageModel
     {
+        public String? FullName { get; set; }
         public void OnGet()
         {
+
+        }
+
+        public void OnPost([FromForm] string name)
+        {
+            FullName = name;
         }
     }
 }
