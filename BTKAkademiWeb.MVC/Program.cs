726c22cd39c3edd278195da3936b03f7edbdf414
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-06 15:27:48 +0300

    Service Extension baslangıcı

--- BTKAkademiWeb.MVC/Program.cs
+++ BTKAkademiWeb.MVC/Program.cs
@@ -1,47 +1,14 @@
-using Entities.Models;
-using Microsoft.EntityFrameworkCore;
-using Repositories;
-using Repositories.Contracts;
-using Services;
-using Services.Contracts;
-using BTKAkademiWeb.MVC.Models;
+using BTKAkademiWeb.MVC.Infrastructe.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<RepositoryContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
-    b => b.MigrationsAssembly("BTKAkademiWeb.MVC"));
-});
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.Name = "BTKAkademiWeb.MVC.Session";
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-});
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-
-builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-
-
-builder.Services.AddScoped<IServiceManager, ServiceManager>();
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<IOrderService, OrderManager>();
-
-builder.Services.AddScoped<Cart>();
-
-builder.Services.AddSingleton<Cart>();
-
-builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureSession();
+builder.Services.ConfigureRepositoryRegistration();
+builder.Services.ConfigureServiceRegistration();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
@@ -49,6 +16,7 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseSession();
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
