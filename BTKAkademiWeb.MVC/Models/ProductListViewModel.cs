6248d1ae76372d971df511139d2ca59f91badb68
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-06 16:32:43 +0300

    Pagination Islemleri

---
+++ BTKAkademiWeb.MVC/Models/ProductListViewModel.cs
@@ -0,0 +1,11 @@
+using Entities.Models;
+
+namespace BTKAkademiWeb.MVC.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+        public Pagination Pagination { get; set; } = new();
+        public int TotalCount => Products.Count();
+    }
+}
