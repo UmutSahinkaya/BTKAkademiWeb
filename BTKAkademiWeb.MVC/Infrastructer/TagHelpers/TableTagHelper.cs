cd679eb58fc7cbcdfa32b025cc5b0cdbc530f6fb
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-06 16:23:39 +0300

    Tag Helpers işlemleri

--- BTKAkademiWeb.MVC/Infrastructer/TagHelpers/TableTagHelper.cs
+++ BTKAkademiWeb.MVC/Infrastructer/TagHelpers/TableTagHelper.cs
@@ -5,6 +5,9 @@ namespace BTKAkademiWeb.MVC.Infrastructe.TagHelpers
     [HtmlTargetElement("table")]
     public class TableTagHelper : TagHelper
     {
-        
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "table table-hover");
+        }
     }
 }
