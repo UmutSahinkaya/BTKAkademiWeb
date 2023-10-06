4abcbc045b673a41da74bec18a4ed8e00ac15588
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-06 16:33:40 +0300

    Pagination Islemleri

--- BTKAkademiWeb.MVC/Controllers/ProductController.cs
+++ BTKAkademiWeb.MVC/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using BTKAkademiWeb.MVC.Models;
+using Entities.Models;
 using Entities.RequestParameters;
 using Microsoft.AspNetCore.Mvc;
 using Repositories;
@@ -18,8 +19,18 @@ namespace BTKAkademiWeb.MVC.Controllers
 
         public IActionResult Index(ProductRequestParameters p)
         {
-            var model = _manager.ProductService.GetAllProductsWithDetails(p);
-            return View(model);
+            var products = _manager.ProductService.GetAllProductsWithDetails(p);
+            var pagination = new Pagination()
+            {
+                CurrenPage = p.PageNumber,
+                ItemsPerPage = p.PageSize,
+                TotalItems = _manager.ProductService.GetAllProducts(false).Count()
+            };
+            return View(new ProductListViewModel()
+            {
+                Products = products,
+                Pagination = pagination
+            });
         }
 
         public IActionResult Get([FromRoute(Name = "id")] int id)
