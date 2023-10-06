98534aa9f0697b15fd4d42175a8a7b9aa3b7416f
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-10-06 16:39:15 +0300

    Pagination Islemleri

--- BTKAkademiWeb.MVC/Infrastructer/TagHelpers/PageLinkTagHelper.cs
+++ BTKAkademiWeb.MVC/Infrastructer/TagHelpers/PageLinkTagHelper.cs
@@ -1,4 +1,9 @@
+
+using BTKAkademiWeb.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace BTKAkademiWeb.MVC.Infrastructe.TagHelpers
@@ -8,9 +13,38 @@ namespace BTKAkademiWeb.MVC.Infrastructe.TagHelpers
     {
         private readonly IUrlHelperFactory _urlHelperFactory;
 
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; }
+
+        public Pagination PageModel { get; set; }
+        public String? PageAction { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
+
         public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
         {
             _urlHelperFactory = urlHelperFactory;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext is not null && PageModel is not null)
+            {
+                IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+                TagBuilder result = new TagBuilder("div");
+                for (int i = 1; i <= PageModel.TotalPages; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { PageNumber = i });
+                    tag.InnerHtml.Append(i.ToString());
+                    result.InnerHtml.AppendHtml(tag);
+                }
+                output.Content.AppendHtml(result.InnerHtml);
+            }
         }
     }
 }
